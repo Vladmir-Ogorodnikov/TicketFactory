904ffb8da42bcc8b916da234aee16a246c727f29
Vladimir <vovinox1110gmail.com>
2026-09-03 08:55:50 +0300

    First Commit

---
+++ ticket.py
@@ -0,0 +1,76 @@
+
+
+from abc import ABC, abstractmethod
+from uuid import uuid4, UUID
+
+class Ticket(ABC):
+
+    def __init__(self, user_id, row, number, price):
+        self.__id = uuid4()
+        self.__user_id = user_id
+        self.__row = row
+        self.__number = number
+        self.__price = price
+
+    @property
+    def row(self):
+        return self.__row
+
+    @property
+    def number(self):
+        return self.__number
+
+    @property
+    def price(self):
+        return self.__price
+
+    @abstractmethod
+    def get_info(self):
+        ...
+
+
+class StandardTicket(Ticket):
+
+    def __init__(self, user_id, row, number, price : int = 10):
+        super().__init__(user_id, row, number, price)
+
+
+
+
+    def get_info(self):
+        return f"Стандартный билет, ряд {self.row()}, место {self.number()}, цена: {self.price()}$"
+
+
+
+class VIPTicket(Ticket):
+
+    def __init__(self, user_id, row, number, price: int = 25):
+        super().__init__(user_id, row, number, price)
+
+
+    def get_info(self):
+        return f"VIP-билет с баром, ряд {self.row}, место {self.number}, цена: {self.price}$"
+
+
+class ChildTicket(Ticket):
+
+    def __init__(self, user_id, row, number, price : int = 5):
+        super().__init__(user_id, row, number, price)
+
+    def get_info(self):
+        return f"Детский билет, ряд {self.row()}, место {self.number()}, цена: {self.price()}$"
+
+
+class TicketFactory:
+
+
+    @staticmethod
+    def create_ticket(user_id : UUID, row : int, number : int, age : int, vip_flag : bool) -> Ticket:
+        if age < 12:
+            return ChildTicket(user_id, row, number)
+        elif vip_flag:
+            return VIPTicket(user_id, row, number)
+        return StandardTicket(user_id, row, number)
+
+ticket = TicketFactory.create_ticket(uuid4(), 5, 11, 10, True)
+print(isinstance(ticket, ChildTicket))
