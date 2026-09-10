f40ffa25eba1b08cdf473e81b1c807f61fbc738b
Vladimir <vovinox1110@gmail.com>
2026-09-10 21:53:34 +0300

    API Pydantic shemas

---
+++ api/shemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from uuid import UUID
+from typing import List, Tuple
+from domain.ticket import Ticket
+
+class TicketResponse(BaseModel):
+    """Pydantic схема для билета в ответе."""
+    ticket_id: UUID
+    ticket_type: str
+    row: int
+    number: int
+    price: float
+
+class PurchaseRequest(BaseModel):
+
+    user_id : UUID
+    seats: List[Tuple[int, int]]
+    age : int
+    vip_flag : bool
+
+class ReceiptResponse(BaseModel):
+    ticket_id : UUID
+    user_id : UUID
+    list_tickets : List[TicketResponse]
+    total_amount : float
+    created_at : str
